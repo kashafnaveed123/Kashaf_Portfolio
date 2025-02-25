2c7e9924bec88ae32d0c9ca29e1f7f1e6cd37730
Kashaf naveed <222kashafnaveed@gmail.com>
2025-02-26 01:13:04 +0500

    adjust deployement error

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,19 +1,41 @@
-"use client"
+// "use client"
 
-import Lottie from "lottie-react";
+// import Lottie from "lottie-react";
+
+// const AnimationLottie = ({ animationPath, width }) => {
+//   const defaultOptions = {
+//     loop: true,
+//     autoplay: true,
+//     animationData: animationPath,
+//     style: {
+//       width: '95%',
+//     }
+//   };
+
+//   return (
+//     <Lottie {...defaultOptions} />
+//   );
+// };
+
+// export default AnimationLottie;
+
+
+
+
+"use client";
+
+import dynamic from "next/dynamic";
+
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
 const AnimationLottie = ({ animationPath, width }) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationPath,
-    style: {
-      width: '95%',
-    }
-  };
-
   return (
-    <Lottie {...defaultOptions} />
+    <Lottie 
+      animationData={animationPath} 
+      loop 
+      autoplay 
+      style={{ width: width || '95%' }} 
+    />
   );
 };
 
